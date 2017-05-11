db4e9e71c413184e30e54360fdf40e9d27cd2ffd
roi shabtai <roi@saturn72.com>
2017-05-11 20:39:09 +0300

    init commit

---
+++ src/SolutionWithGitSubmodulesAndNuget/SimpeleConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpeleConsoleApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Do what you have to do.");
+            Console.WriteLine("Just press any key when you're done...");
+            Console.ReadKey();
+        }
+    }
+}
